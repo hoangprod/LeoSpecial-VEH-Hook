7814f4c8cec8d8df4e7e7f5cb8dda7aec8e8e3e4
hoangprod <41701929+hoangprod@users.noreply.github.com>
2019-01-13 14:54:44 -0500

    Rename LeoSpecial.cpp to Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,50 @@
+﻿#include "LeoSpecial.h"
+#include <time.h>
+
+void(WINAPI *o_Sleep)(DWORD dwMilliseconds) = Sleep;
+
+
+void hk_sleep(DWORD dwMilliseconds)
+{
+	printf("Hooked!\n");
+	return;
+}
+
+int main()
+{
+	//Hook codes!
+	LeoHook Leo;
+	if (!Leo.Hook((uintptr_t)Sleep, (uintptr_t)hk_sleep))
+		printf("[-] Failed to hook...\n");
+
+	double time_spent = 0.0;
+	clock_t begin = clock();
+
+	//Hooked
+	Sleep(100000);
+
+
+	clock_t end = clock();
+	time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("Time elpased is %f seconds\n", time_spent);
+
+	//Unhook
+	if (!Leo.Unhook())
+		printf("[-] Failed to unhook...\n");
+
+	time_spent = 0.0;
+	begin = clock();
+
+	Sleep(1000);
+
+
+	end = clock();
+	time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("Time elpased is %f seconds\n", time_spent);
+
+
+	std::cin.get();
+    return 0;
+}
+
+
