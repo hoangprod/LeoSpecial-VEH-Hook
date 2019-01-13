1ebe1b8301c3f19997bf95b1a435b01c66140f69
hoangprod <41701929+hoangprod@users.noreply.github.com>
2019-01-13 16:50:08 -0500

    Update LeoSpecial.h

--- LeoSpecial.h
+++ LeoSpecial.h
@@ -33,14 +33,17 @@ DWORD LeoHook::oldProtection = 0;
 
 bool LeoHook::Hook(uintptr_t original_fun, uintptr_t hooked_fun)
 {
-	og_fun = original_fun;
+	LeoHook::og_fun = original_fun;
 	LeoHook::hk_fun = hooked_fun;
 
+	//We cannot hook two functions in the same page, because we will cause an infinite callback
 	if (AreInSamePage((const uint8_t*)og_fun, (const uint8_t*)hk_fun))
 		return false;
 
+	//Register the Custom Exception Handler
 	VEH_Handle = AddVectoredExceptionHandler(true, (PVECTORED_EXCEPTION_HANDLER)LeoHandler);
 
+	//Toggle PAGE_GUARD flag on the page
 	if(VEH_Handle && VirtualProtect((LPVOID)og_fun, 1, PAGE_EXECUTE_READ | PAGE_GUARD, &oldProtection))
 		return true;
 	
@@ -50,9 +53,9 @@ bool LeoHook::Hook(uintptr_t original_fun, uintptr_t hooked_fun)
 bool LeoHook::Unhook()
 {
 	DWORD old;
-	if (VEH_Handle &&
-		VirtualProtect((LPVOID)og_fun, 1, oldProtection, &old) &&
-		RemoveVectoredExceptionHandler(VEH_Handle))
+	if (VEH_Handle && //Make sure we have a valid Handle to the registered VEH
+		VirtualProtect((LPVOID)og_fun, 1, oldProtection, &old) && //Restore old Flags
+		RemoveVectoredExceptionHandler(VEH_Handle)) //Remove the VEH
 		return true;
 
 	return false;
@@ -60,41 +63,40 @@ bool LeoHook::Unhook()
 
 LONG WINAPI LeoHook::LeoHandler(EXCEPTION_POINTERS *pExceptionInfo)
 {
-	if (pExceptionInfo->ExceptionRecord->ExceptionCode == STATUS_GUARD_PAGE_VIOLATION)
+	if (pExceptionInfo->ExceptionRecord->ExceptionCode == STATUS_GUARD_PAGE_VIOLATION) //We will catch PAGE_GUARD Violation
 	{
-		if (pExceptionInfo->ContextRecord->XIP == (uintptr_t)og_fun)
+		if (pExceptionInfo->ContextRecord->XIP == (uintptr_t)og_fun) //Make sure we are at the address we want within the page
 		{
-			pExceptionInfo->ContextRecord->XIP = (uintptr_t)hk_fun;
+			pExceptionInfo->ContextRecord->XIP = (uintptr_t)hk_fun; //Modify EIP/RIP to where we want to jump to instead of the original function
 		}
 
-		pExceptionInfo->ContextRecord->EFlags |= 0x100;
-		return EXCEPTION_CONTINUE_EXECUTION;
+		pExceptionInfo->ContextRecord->EFlags |= 0x100; //Will trigger an STATUS_SINGLE_STEP exception right after the next instruction get executed. In short, we come right back into this exception handler 1 instruction later
+		return EXCEPTION_CONTINUE_EXECUTION; //Continue to next instruction
 	}
 
-	if (pExceptionInfo->ExceptionRecord->ExceptionCode == STATUS_SINGLE_STEP)
+	if (pExceptionInfo->ExceptionRecord->ExceptionCode == STATUS_SINGLE_STEP) //We will also catch STATUS_SINGLE_STEP, meaning we just had a PAGE_GUARD violation
 	{
 		DWORD dwOld;
-		VirtualProtect((LPVOID)og_fun, 1, PAGE_EXECUTE_READ | PAGE_GUARD, &dwOld);
+		VirtualProtect((LPVOID)og_fun, 1, PAGE_EXECUTE_READ | PAGE_GUARD, &dwOld); //Reapply the PAGE_GUARD flag because everytime it is triggered, it get removes
 
-		return EXCEPTION_CONTINUE_EXECUTION;
+		return EXCEPTION_CONTINUE_EXECUTION; //Continue the next instruction
 	}
 
-	return EXCEPTION_CONTINUE_SEARCH;
+	return EXCEPTION_CONTINUE_SEARCH; //Keep going down the exception handling list to find the right handler IF it is not PAGE_GUARD nor SINGLE_STEP
 }
-
 
 bool LeoHook::AreInSamePage(const uint8_t* Addr1, const uint8_t* Addr2)
 {
 	MEMORY_BASIC_INFORMATION mbi1;
-	if (!VirtualQuery(Addr1, &mbi1, sizeof(mbi1)))
+	if (!VirtualQuery(Addr1, &mbi1, sizeof(mbi1))) //Get Page information for Addr1
 		return true;
 
 	MEMORY_BASIC_INFORMATION mbi2;
-	if (!VirtualQuery(Addr2, &mbi2, sizeof(mbi2)))
+	if (!VirtualQuery(Addr2, &mbi2, sizeof(mbi2))) //Get Page information for Addr1
 		return true;
 
-	if (mbi1.BaseAddress == mbi2.BaseAddress)
-		return true;
+	if (mbi1.BaseAddress == mbi2.BaseAddress) //See if the two pages start at the same Base Address
+		return true; //Both addresses are in the same page, abort hooking!
 
-	return false;
+	return false; 
 }
